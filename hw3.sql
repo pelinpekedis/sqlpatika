ad0bbd24b3b56d7c04f2913f150ca8e9dbc6c8a1
pelinpekedis <115707636+pelinpekedis@users.noreply.github.com>
2022-11-28 22:57:38 +0300

    Update hw3.sql

--- hw3.sql
+++ hw3.sql
@@ -6,3 +6,5 @@ SELECT country FROM country WHERE country LIKE '%_____n';
 SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
 --Q4:z
 SELECT * FROM film WHERE (title LIKE 'C%') AND (length>90) AND (rental_rate=2.99);
+
+--www.patika.dev
